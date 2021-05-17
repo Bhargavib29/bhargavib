be580fd43494e4e0153015d197958198004ccd95
Bhargavib29 <84303801+Bhargavib29@users.noreply.github.com>
2021-05-17 10:40:44 +0530

    Add files via upload

---
+++ c1.java
@@ -0,0 +1,17 @@
+public class1
+{
+private static int i=0;
+private static int j=0;
+public static void main(string []args)
+{
+int i=4;
+int k=5;
+{
+int j=3;
+System.out .println("i+j is "+i +j);
+}
+k=i+j;
+System.out.println(k);
+System.out.println(j);
+}
+}
